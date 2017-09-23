348f94d53a66e52e2521abf2467905a2652eac08
nlincoln <nlincoln@intellifarms.com>
2017-09-22 23:14:50 -0500

    Domain relative graphql endpoint

--- frontend/src/components/App/apollo-client/index.js
+++ frontend/src/components/App/apollo-client/index.js
@@ -3,7 +3,7 @@ import { ApolloClient, createNetworkInterface } from 'react-apollo';
 import { ApolloProvider as Provider } from 'react-apollo';
 
 const networkInterface = createNetworkInterface({
-  uri: 'http://localhost:4000/api/graphql/graphql'
+  uri: '/api/graphql/graphql'
 });
 
 export const client = new ApolloClient({
